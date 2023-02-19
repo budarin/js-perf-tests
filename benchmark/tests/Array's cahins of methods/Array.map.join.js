9a5d717196952740b1f5eb305fefb8cde3bdecb2
Вадим Бударин <budarin@users.noreply.github.com>
2023-02-19 22:41:00 +0300

    Update Array.map.join.js

--- benchmark/tests/Array's cahins of methods/Array.map.join.js
+++ benchmark/tests/Array's cahins of methods/Array.map.join.js
@@ -1,7 +1,7 @@
 function arrayMapJoinHelper(arrayObject, mapPredicate, separator = ',') {
     var i = 0;
     var len = arrayObject.length;
-    var result = String(mapPredicate(arrayObject[0], 0));
+    var result = mapPredicate(arrayObject[0], 0) || '';
 
     while (++i < len) {
         result = result + separator + String(mapPredicate(arrayObject[i], i));
